5de164449fcaf01d200a6d2e9a4db0e6816a2d87
XiaoYiPeng <helloxyp@gmail.com>
2016-04-23 15:31:14 +0800

    Initial Commit

---
+++ PageViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PageViewDemo
+//
+//  Created by XiaoYiPeng on 16/4/23.
+//  Copyright © 2016年 XiaoYiPeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
